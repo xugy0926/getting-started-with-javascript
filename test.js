5d4ab68d0dfefcd817f34c4131ae5edf38323212
dlily001 <865590627@qq.com>
2017-08-23 15:57:00 +0800

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-var flag =0;function personA()
-{       
-     flag = flag + 1;    
-     console.log(flag);   
-      if(flag < 100 ){   
-               personA(); 
-                  }    }
-                  personA(); 
