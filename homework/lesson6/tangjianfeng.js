2314f3eff2854ff67967f932a7d05fc8cbf8c321
Tony <tangvwin@gmail.com>
2017-08-27 22:55:19 +0800

    lesson6

---
+++ homework/lesson6/tangjianfeng.js
@@ -0,0 +1,22 @@
+var fs = require('fs');
+
+var dirPathString = '../../homework/lesson1';
+
+var outputPathString = './write_lesson1_sync.txt';
+var files = fs.readdirSync('../../homework/lesson1')
+
+function callback(err, files) {
+    if (err) {
+         console.log('读取文件失败');
+    }
+    else if (files.length >0) {
+        console.log(files);
+    } else {
+        console.log('没有找到任何文件');
+    }
+}
+
+
+
+fs.readdir(dirPathString, callback);
+fs.writeFileSync(outputPathString,files);
