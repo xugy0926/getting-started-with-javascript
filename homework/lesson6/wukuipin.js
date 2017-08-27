5ad77a31eb2886872eab67a2dac573cc538634ed
Antarts <m.dream@foxmail.com>
2017-08-28 01:18:51 +0800

    lesson6 homework version of No.3 by wukuipin

--- homework/lesson6/wukuipin.js
+++ homework/lesson6/wukuipin.js
@@ -10,6 +10,7 @@ function callbackRead(err, files) {
   }
   if (files.length > 0) {
     console.log(files);
+    fileContent(files);
   } else {
     console.log('没有找到任何文件');
   }
@@ -23,7 +24,12 @@ function callbackWrite(err) {
   }
 }
 
+function fileContent(files) {
+  fileContent = files;
+  fs.writeFile(filePathString, fileContent, callbackWrite);
+}
+
 fs.readdir(dirPathString, callbackRead);
-fs.writeFile(filePathString, callbackRead, callbackWrite);
+
 
 // 第一次提交的代码，在思考怎么写时脑中一直想着变量，所以写了看起来奇怪的代码。
