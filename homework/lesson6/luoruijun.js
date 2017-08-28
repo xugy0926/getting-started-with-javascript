e74b4e06b27b1899b8e9b664742cddc2358c3c3f
sunrise81 <luoruijunblue@hotmail.com>
2017-08-28 08:43:08 +0800

    luoruijun-homeworklesson6

--- homework/lesson6/luoruijun.js
+++ homework/lesson6/luoruijun.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 var dirPathString = '../../homework/lesson1';
-
+var filePathString = './write_file_async.txt';
 function callback(err, files) {
   if (err) {
     console.log('读取文件失败');
@@ -8,15 +8,22 @@ function callback(err, files) {
 
   if (files.length > 0) {
     console.log(files);
+        fs.writeFile(filePathString, files, callback1);
   } else {
     console.log('没有找到任何文件');
   }
 }
 
+function callback1(err) {
+    if (err) {
+      console.log('写文件失败');
+    } else {
+      console.log('写文件成功');
+    }
+  }
 
 function scanAndWriteFiles_async(callback){
     fs.readdir(dirPathString, callback);
-    
 }
 
 scanAndWriteFiles_async(callback);
