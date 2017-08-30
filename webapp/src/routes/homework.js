92b2123b5172143ef2757be9b72df038226d7c92
young <young_xu@qq.com>
2017-08-30 19:42:01 +0800

    update homework.js

--- webapp/src/routes/homework.js
+++ webapp/src/routes/homework.js
@@ -5,12 +5,13 @@ import { get } from '../request';
 var router = express.Router();
 
 /* GET homework page. */
-router.get('/:id', async function(req, res, next) {
+router.get('/:number', async function(req, res, next) {
   const id = req.params.id;
+  const number = req.params.number;
 
   try {
     let result = await get({
-      url: `${apiUrl}/learnJS/course/${id}/homework/${id}`
+      url: `${apiUrl}/learnJS/course/1/homework/${number}`
     });
 
     res.render('homework', { title: '第' + id + '次作业', homeworks: result.homeworks });
