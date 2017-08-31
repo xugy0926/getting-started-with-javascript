7c339d655ed06f967f2785b585325a7b64b7233d
risexie <rise_xie@sina.com>
2017-08-31 22:38:17 +0800

    merge upstream

--- homework/lesson6/Risexie.js
+++ homework/lesson6/Risexie.js
@@ -13,6 +13,12 @@ function callback1(err,files){
     }
  
     if(files.length > 0) {
+//在使用不同的方法获取数据的时候要注意，获取数据的格式是什么
+// 例如 fs.writeFile这个方法输出的内容是是一数组。
+//所以在下面用了.join这个方法，（.jion）这个方法可以将数组中的所有元素放进一个字符串
+//所以输出的file数组，就被.jion方法放进了fileContent。
+//.jion这个方法还可以在后面给他指定元素的分隔符，这里用了“\n”。通过空行分割。
+//也可以用var = fileContent = files.jion(".")。用.来进行分割
         console.log(files);
  //把files里面的文件赋值给fileContent       
        var fileContent = files.join("\n")
@@ -28,6 +34,13 @@ function callback1(err,files){
 }
 
 //调用fs的readdir函数，并输出文档。
+//在调用任何函数之前，我们一定要了解清楚函数所需要输入的的参数类型是什么
+//例如在下面的函数fs.readdir(path,function(err,files))
+//第一个地方是读取文件的路径，第二个是一个回调函数，里面包含错误的信心和files
+//也就是读取出来的数组！，一定要注意是数组，这一点非常重要。
+//因为我们用写入函数的时候，fs.writeFile这个函数要读取的参数是字符串
+//所以我们需要使用别的方法把数组转换成字符串，这样才能被fs.write这个函数使用
+
 fs.readdir(dirPathString,callback1)
 
 
