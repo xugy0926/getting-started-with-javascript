9f78f7cd322c57d328846b6b3daabaaf8b776279
zhanggaoyanggordon <zhanggaoyanggordon@163.com>
2017-10-20 18:16:13 +0800

    firstSix3

--- homework/lesson6/zhanggaoyang.js
+++ homework/lesson6/zhanggaoyang.js
@@ -6,8 +6,6 @@ var dirPathStiring = '../my_project/words-from-the-heart';
 var files = fs.readdirSync(dirPathStiring);
 console.log(files)
 
-var fs = require('fs')
-
 var outputPathString = './zhanggaoyang.txt';
 
 var fileContent = files
